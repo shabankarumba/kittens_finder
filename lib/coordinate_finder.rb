1c18ac0d50587182996d370bfd84f74c114472df
Shaban Karumba <shaban.karumba@fundingcircle.com>
2018-01-23 09:17:48 +0000

    Find coordinates based on directions given to the Coordinate finder

---
+++ lib/coordinate_finder.rb
@@ -0,0 +1,60 @@
+class CoordinateFinder
+  def initialize(directions)
+    @directions = directions
+  end
+
+  def call  
+    coordinates = Coordinates.new(x: 0, y: 0)
+    
+    directions.map do |direction|
+      coordinates.public_send(direction.to_sym)
+    end
+
+    { x: coordinates.x, y: coordinates.y }
+  end
+
+  private
+
+  attr_reader :directions
+
+  class Coordinates
+    attr_reader :x, :y
+    def initialize(x: 0, y: 0, turnings: 0)
+      @x = x
+      @y = y
+      @turnings = turnings
+    end
+  
+    def facing
+      directions[turnings % directions.count]
+    end
+
+    def left
+      @turnings -= 1
+    end
+  
+    def right
+      @turnings += 1
+    end
+
+    def forward
+      case facing
+      when :left
+        @x -= 1
+      when :right
+        @x += 1
+      when :forward
+        @y += 1
+      when :backward
+        @y -= 1
+      end
+    end
+
+    def directions
+      [:forward, :right, :backward, :left]
+    end
+
+    private
+    attr_reader :turnings
+  end
+end
---
+++ spec/lib/coordinate_finder_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require_relative '../../lib/coordinate_finder.rb'
+
+RSpec.describe CoordinateFinder do
+  let(:directions) do
+    [ 
+      'forward', 'right', 'forward', 'forward', 
+      'forward', 'left', 'forward', 'forward', 'left', 
+      "right", "forward", "right", "forward", "forward", 
+      "right", "forward", "forward", "left"
+    ]
+  end
+  subject { described_class.new(directions) }
+
+  describe '#call' do
+    it 'outputs the coordinates of the kittens' do
+      expect(subject.call).to eq({ x: 5, y: 2 })
+    end
+  end
+end
